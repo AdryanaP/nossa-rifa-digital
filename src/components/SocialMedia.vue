a3c9c195d2eb07d323f6018ec6580b5bc9a5ba98
adryana portugal <dryportuga@gmail.com>
2023-04-01 17:19:50 -0300

    feat: atualizar redes sociais

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -7,14 +7,15 @@
       <form class="my-4 flex flex-col gap-8">
         <div class="space-y-8">
           <div>
-            <label for="wpp" class="block font-medium text-sm">Grupo no WhatsApp</label>
+            <label for="wpp" class="block font-medium text-sm"
+              >Grupo no WhatsApp</label
+            >
             <div class="mt-1">
               <input
                 id="wpp"
                 name="wpp"
-                v-model="socialMedias.wpp"
+                v-model="socialMedias.whatsapp"
                 type="text"
-                required=""
                 class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
               />
             </div>
@@ -29,21 +30,21 @@
                 name="telegram"
                 v-model="socialMedias.telegram"
                 type="text"
-                required=""
                 class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
               />
             </div>
           </div>
 
           <div>
-            <label for="instagram" class="block font-medium text-sm">Perfil no Instagram</label>
+            <label for="instagram" class="block font-medium text-sm"
+              >Perfil no Instagram</label
+            >
             <div class="mt-1">
               <input
                 id="instagram"
                 name="instagram"
                 v-model="socialMedias.instagram"
                 type="text"
-                required=""
                 class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
               />
             </div>
@@ -59,7 +60,6 @@
                 name="tiktok"
                 v-model="socialMedias.tiktok"
                 type="text"
-                required=""
                 class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
               />
             </div>
@@ -74,42 +74,84 @@
                 name="youtube"
                 v-model="socialMedias.youtube"
                 type="text"
-                required=""
                 class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
               />
             </div>
           </div>
         </div>
 
-
         <div class="text-right">
           <button
             class="rounded-md bg-black text-white px-4 py-3 text-sm font-medium shadow-sm focus:outline-none"
-            type="submit"
+            type="button"
+            @click="updateSocials"
           >
             Salvar
           </button>
         </div>
       </form>
     </div>
+    <AlertError
+      v-if="hasError"
+      :data="msgError"
+      class="transition-opacity ease-in duration-700 animate-fade"
+    />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import AlertError from "@/components/AlertError.vue";
 
 export default {
   name: "MyAccount",
+  components: { AlertError },
   data() {
     return {
-      socialMedias: {
-        wpp: "",
-        telegram: "",
-        instagram: "",
-        tiktok: "",
-        youtube: "",
-
-      },
+      socialMedias: {},
+      msgError: "",
+      hasError: false,
     };
+  },
+  methods: {
+    updateSocials() {
+      const token = sessionStorage.getItem("token");
+
+      axios
+        .post(
+          "https://api.nossarifadigital.digital/v1/resources/settings/social",
+          {
+            social_whatsapp: this.socialMedias.whatsapp,
+            social_telegram: this.socialMedias.telegram,
+            social_instagram: this.socialMedias.instagram,
+            social_tiktok: this.socialMedias.tiktok,
+            social_youtube: this.socialMedias.youtube,
+            device_name: "web",
+          },
+          {
+            headers: {
+              Accept: "application/json",
+              Authorization: "Bearer " + token,
+            },
+          }
+        )
+        .then((res) => {
+          sessionStorage.setItem("user", JSON.stringify(res.data.user));
+          window.history.go();
+        })
+
+        .catch((error) => {
+          this.msgError = error.response.data.message;
+          this.hasError = true;
+          setTimeout(() => {
+            this.hasError = false;
+          }, 6000);
+        });
+    },
+  },
+  created() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    this.socialMedias = user.social;
   },
 };
 </script>
